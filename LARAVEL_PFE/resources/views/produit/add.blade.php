d44efd9254c2a7c50f01783c32cab25badca7031
REACT <aitlhcenredouanwac@gmail.com>
2023-05-21 18:20:48 +0000

    fix dropzone

--- LARAVEL_PFE/resources/views/produit/add.blade.php
+++ LARAVEL_PFE/resources/views/produit/add.blade.php
@@ -80,12 +80,12 @@
           <textarea class="form-control" id="description" placeholder="Enter the product description" name="" id=""
               cols="30" rows="10"></textarea>
       </div>
+      <br>
       <div class="row">
         <div class="col-md-12">
-          <label>Description</label>
           <div class="card card-default">
             <div class="card-header">
-              <h3 class="card-title">Product Images</h3>
+              <h3 class="card-title">Images</h3>
             </div>
             <div class="card-body">
               <div id="actions" class="row">
@@ -95,23 +95,10 @@
                       <i class="fas fa-plus"></i>
                       <span>Add files</span>
                     </span>
-                    <button type="submit" class="btn btn-primary col start">
-                      <i class="fas fa-upload"></i>
-                      <span>Start upload</span>
-                    </button>
-                    <button type="reset" class="btn btn-warning col cancel">
-                      <i class="fas fa-times-circle"></i>
-                      <span>Cancel upload</span>
-                    </button>
+                   
                   </div>
                 </div>
-                <div class="col-lg-6 d-flex align-items-center">
-                  <div class="fileupload-process w-100">
-                    <div id="total-progress" class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-                      <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
-                    </div>
-                  </div>
-                </div>
+                
               </div>
               <div class="table table-striped files" id="previews">
                 <div id="template" class="row mt-2">
@@ -123,23 +110,13 @@
                         <span class="lead" data-dz-name></span>
                         (<span data-dz-size></span>)
                       </p>
-                      <strong class="error text-danger" data-dz-errormessage></strong>
+                      
                   </div>
                   <div class="col-4 d-flex align-items-center">
-                      <div class="progress progress-striped active w-100" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-                        <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
-                      </div>
+                      <div></div>
                   </div>
                   <div class="col-auto d-flex align-items-center">
                     <div class="btn-group">
-                      <button class="btn btn-primary start">
-                        <i class="fas fa-upload"></i>
-                        <span>Start</span>
-                      </button>
-                      <button data-dz-remove class="btn btn-warning cancel">
-                        <i class="fas fa-times-circle"></i>
-                        <span>Cancel</span>
-                      </button>
                       <button data-dz-remove class="btn btn-danger delete">
                         <i class="fas fa-trash"></i>
                         <span>Delete</span>
@@ -149,19 +126,19 @@
                 </div>
               </div>
             </div>
-            <!-- /.card-body -->
-            
+          
           </div>
           <!-- /.card -->
         </div>
       </div>
-   
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <br>
+      <button  type="submit" class="btn btn-primary">Submit</button>
+
+     
   </form>
- 
       </div>
       <script>
-         Dropzone.autoDiscover = false
+       Dropzone.autoDiscover = false
 
 // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
 var previewNode = document.querySelector("#template")
@@ -180,22 +157,22 @@ var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
   clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
 })
 
-myDropzone.on("addedfile", function(file) {
+//myDropzone.on("addedfile", function(file) {
   // Hookup the start button
-  file.previewElement.querySelector(".start").onclick = function() { myDropzone.enqueueFile(file) }
-})
+ // file.previewElement.querySelector(".start").onclick = function() { myDropzone.enqueueFile(file) }
+//})
 
 // Update the total progress bar
-myDropzone.on("totaluploadprogress", function(progress) {
-  document.querySelector("#total-progress .progress-bar").style.width = progress + "%"
-})
+//myDropzone.on("totaluploadprogress", function(progress) {
+ // document.querySelector("#total-progress .progress-bar").style.width = progress + "%"
+//})
 
-myDropzone.on("sending", function(file) {
+//myDropzone.on("sending", function(file) {
   // Show the total progress bar when upload starts
-  document.querySelector("#total-progress").style.opacity = "1"
+ // document.querySelector("#total-progress").style.opacity = "1"
   // And disable the start button
-  file.previewElement.querySelector(".start").setAttribute("disabled", "disabled")
-})
+  //file.previewElement.querySelector(".start").setAttribute("disabled", "disabled")
+//})
 
 // Hide the total progress bar when nothing's uploading anymore
 myDropzone.on("queuecomplete", function(progress) {
@@ -205,15 +182,12 @@ myDropzone.on("queuecomplete", function(progress) {
 // Setup the buttons for all transfers
 // The "add files" button doesn't need to be setup because the config
 // `clickable` has already been specified.
-document.querySelector("#actions .start").onclick = function() {
-  myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED))
-}
+//document.querySelector("#actions .start").onclick = function() {
+  //myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED))
+//}
 document.querySelector("#actions .cancel").onclick = function() {
   myDropzone.removeAllFiles(true)
 }
-      </script>
-     
-            
-              
+      </script>    
 @endsection
       
