2d26a0a16c371c2381c085ac6d3e4c891922398f
REACT <aitlhcenredouanwac@gmail.com>
2023-06-05 21:18:28 +0000

    ..

--- LARAVEL_PFE/resources/views/admin.blade.php
+++ LARAVEL_PFE/resources/views/admin.blade.php
@@ -273,12 +273,7 @@
                                 </p>
                             </a>
                             <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="{{ route('user.index') }}" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Afficher les Users </p>
-                                    </a>
-                                </li>
+                                
                             </ul>
                         </li>
                     </ul>
--- LARAVEL_PFE/routes/web.php
+++ LARAVEL_PFE/routes/web.php
@@ -20,7 +20,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::view("/admin","admin")->name("admin");
+Route::get("/",function (){
+    return view("admin");
+});
 Route::view("/layout","layouts");
 
 // Route::get("/compte",[UserController::class,'compte']);
