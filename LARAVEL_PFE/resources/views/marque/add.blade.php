9faca5f688be23b00c1bd7e051923eac924d89f1
REACT <aitlhcenredouanwac@gmail.com>
2023-06-05 13:09:53 +0000

    ..

---
+++ LARAVEL_PFE/resources/views/marque/add.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts')
+@section("title")
+   add categorie
+@endsection
+@section("content")
+<div class="content-wrapper p-4" >
+    <div class="row">
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h3>150</h3>
+
+              <p>New Orders</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+          <div class="small-box bg-success">
+            <div class="inner">
+              <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+              <p>Bounce Rate</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+          <div class="small-box bg-warning">
+            <div class="inner">
+              <h3>44</h3>
+
+              <p>User Registrations</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+          <div class="small-box bg-danger">
+            <div class="inner">
+              <h3>65</h3>
+
+              <p>Unique Visitors</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+      @if(session()->has("success"))
+    <div class="alert alert-info">
+      <b>Added Successfully !!</b>
+    </div>
+    @endif
+        <form action="{{route('category.store')}}" method="POST"> 
+          @csrf
+        <div class="form-group">
+          <label>marque</label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="categorie" placeholder="Entrer Un categorie" name="marque">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <a  class="btn btn-info" href="{{route('category.index')}}">afficher Les Categories</a>
+    </form>
+</div>
+@endsection
